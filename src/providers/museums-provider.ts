029f039f3f6ceb44a0910c1db9fca73225fbea58
sarunas-k <sarunas101@gmail.com>
2017-04-20 07:29:54 +0300

    Change image path

--- src/providers/museums-provider.ts
+++ src/providers/museums-provider.ts
@@ -33,70 +33,70 @@ export class MuseumsProvider {
         description: 'Aprašymas.',
         lat: 54.8929806,
         lng: 23.9251271,
-        imageUrl: '../assets/images/pic.jpg'
+        imageUrl: 'assets/images/pic.jpg'
       },
       {
         name: 'M. K. Ciurlionio dailes muziejus',
         description: 'Aprašymas.',
         lat: 54.9003558,
         lng: 23.9109429,
-        imageUrl: '../assets/images/pic.jpg'
+        imageUrl: 'assets/images/pic.jpg'
       },
       {
         name: 'Vytauto Didžiojo karo muziejus	',
         description: 'Aprašymas.',
         lat: 54.8998822,
         lng: 23.9120006,
-        imageUrl: '../assets/images/pic.jpg'
+        imageUrl: 'assets/images/pic.jpg'
       },
       {
         name: 'Tado Ivanausko zoologijos muziejus',
         description: 'Aprašymas.',
         lat: 54.898067,
         lng: 23.903934,
-        imageUrl: '../assets/images/pic.jpg'
+        imageUrl: 'assets/images/pic.jpg'
       },
       {
         name: 'Kauno tvirtovės VII fortas',
         description: 'Aprašymas.',
         lat: 54.915513,
         lng: 23.9282227,
-        imageUrl: '../assets/images/pic.jpg'
+        imageUrl: 'assets/images/pic.jpg'
       },
       {
         name: 'Lietuvos Liaudies Buities Muziejus',
         description: 'Aprašymas.',
         lat: 54.866405,
         lng: 24.201013,
-        imageUrl: '../assets/images/pic.jpg'
+        imageUrl: 'assets/images/pic.jpg'
       },
       {
         name: 'Lietuvos aviacijos muziejus',
         description: 'Aprašymas.',
         lat: 54.8778491,
         lng: 23.8900917,
-        imageUrl: '../assets/images/pic.jpg'
+        imageUrl: 'assets/images/pic.jpg'
       },
       {
         name: 'Kauno IX forto muziejus	',
         description: 'Aprašymas.',
         lat: 54.943955,
         lng: 23.875282,
-        imageUrl: '../assets/images/pic.jpg'
+        imageUrl: 'assets/images/pic.jpg'
       },
       {
         name: 'Lietuvos dailės muziejus',
         description: 'Aprašymas.',
         lat: 54.6806047,
         lng: 25.288688,
-        imageUrl: '../assets/images/pic.jpg'
+        imageUrl: 'assets/images/pic.jpg'
       },
       {
         name: 'Kauno rajono muziejus',
         description: 'Aprašymas.',
         lat: 54.9431792,
         lng: 23.7844911,
-        imageUrl: '../assets/images/pic.jpg'
+        imageUrl: 'assets/images/pic.jpg'
       }
     ];
   }
